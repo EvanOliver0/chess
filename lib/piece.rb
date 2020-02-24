0c52565bcb87909ca146d0c02d83d6614817a3a6
EvanOliver0 <ewoliver@protonmail.com>
2020-02-23 19:50:50 -0500

    Add factory methods for pieces to Piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -12,4 +12,34 @@ class Piece
   def move
     @has_moved = true
   end
+
+  def self.pawn(color:)
+    symbol = color == "white" ? "♙" : "♟"
+    return self.new(name: "pawn", color: color, symbol: symbol, value: 1)
+  end
+
+  def self.knight(color:)
+    symbol = color == "white" ? "♘" : "♞"
+    return self.new(name: "knight", color: color, symbol: symbol, value: 3)
+  end
+
+  def self.bishop(color:)
+    symbol = color == "white" ? "♗" : "♝"
+    return self.new(name: "bishop", color: color, symbol: symbol, value: 3)
+  end
+
+  def self.rook(color:)
+    symbol = color == "white" ? "♖" : "♜"
+    return self.new(name: "rook", color: color, symbol: symbol, value: 5)
+  end
+
+  def self.queen(color:)
+    symbol = color == "white" ? "♕" : "♛"
+    return self.new(name: "queen", color: color, symbol: symbol, value: 9)
+  end
+
+  def self.king(color:)
+    symbol = color == "white" ? "♔" : "♚"
+    return self.new(name: "king", color: color, symbol: symbol, value: 0)
+  end
 end
