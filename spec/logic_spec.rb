b9f2bfc62bb235b4a582c4c1a8c3308b52e2e4b5
EvanOliver0 <ewoliver@protonmail.com>
2020-04-06 13:27:47 -0400

    Create specs for piece move functions

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,20 +2,50 @@ require "./lib/logic.rb"
 
 describe Logic do
   describe "#bishop_moves" do
-    it "returns all & only legal moves for a bishop with no pieces blocking it" do
+    before(:each) do
+      @spaces = Array.new(8) { Array.new(8) }
 
+      @subject = instance_double("piece")
+      allow(@subject).to receive(:color).and_return("white")
+    end
+
+    it "returns all & only legal moves for a bishop with no pieces blocking it" do
+      expected = [[0, 1], [1, 2], [2, 3], \
+                  [2, 5], [1, 6], [0, 7], \
+                  [4, 5], [5, 6], [6, 7], \
+                  [4, 3], [5, 2], [6, 1], [7, 0]]
+
+      moves = Logic.bishop_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a bishop blocked by allied pieces" do
+      @spaces = setup( [ [2, 3], [1, 6], [6, 7], [7, 0] ], "white" )
+      expected = [[2, 5], \
+                  [4, 5], [5, 6], \
+                  [4, 3], [5, 2], [6, 1]]
 
+      moves = Logic.bishop_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a bishop that can take enemy pieces" do
+      @spaces = setup( [ [2, 3], [1, 6], [6, 7], [7, 0] ], "black" )
+      expected = [[2, 3], \
+                  [2, 5], [1, 6], \
+                  [4, 5], [5, 6], [6, 7], \
+                  [4, 3], [5, 2], [6, 1], [7, 0]]
 
+      moves = Logic.bishop_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns an empty array for a bishop with no legal moves" do
+      @spaces = setup( [ [0, 1], [1, 1], [1, 0] ], "white" )
+      expected = []
 
+      moves = Logic.bishop_moves(@spaces, [0, 0], @subject)
+      expect(moves).to match_array(expected)
     end
   end
 
@@ -88,46 +118,113 @@ describe Logic do
   end
 
   describe "#king_moves" do
-    it "returns all & only legal moves for a king with no pieces blocking it" do
+    before(:each) do
+      @spaces = Array.new(8) { Array.new(8) }
 
+      @subject = instance_double("piece")
+      allow(@subject).to receive(:color).and_return("white")
+    end
+
+    it "returns all & only legal moves for a king with no pieces blocking it" do
+      expected = [[2, 3], [2, 4], [2, 5], \
+                  [3, 3], [3, 5], \
+                  [4, 3], [4, 4], [4, 5]]
+
+      moves = Logic.king_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
+    end
+
+    it "returns all & only legal moves for a king near the edge of the board" do
+      expected = [[3, 0], [3, 1], \
+                  [4, 1], \
+                  [5, 0], [5, 1]]
+
+      moves = Logic.king_moves(@spaces, [4, 0], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a king blocked by allied pieces" do
+      @spaces = setup( [ [2, 3], [3, 5], [4, 5] ], "white" )
+      expected = [[2, 4], [2, 5], \
+                  [3, 3], \
+                  [4, 3], [4, 4]]
 
+      moves = Logic.king_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a king that can take enemy pieces" do
+      @spaces = setup( [ [2, 3], [3, 5], [4, 5] ], "black" )
+      expected = [[2, 3], [2, 4], [2, 5], \
+                  [3, 3], [3, 5], \
+                  [4, 3], [4, 4], [4, 5]]
 
+      moves = Logic.king_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns an empty array for a king with no legal moves" do
+      @spaces = setup( [ [3, 0], [3, 1], [4, 1], [5, 0], [5, 1] ], "white" )
+      expected = []
 
+      moves = Logic.king_moves(@spaces, [4, 0], @subject)
+      expect(moves).to match_array(expected)
     end
   end
 
   describe "#knight_moves" do
-    it "returns all & only legal moves for a knight with no pieces blocking it" do
+    before(:each) do
+      @spaces = Array.new(8) { Array.new(8) }
 
+      @subject = instance_double("piece")
+      allow(@subject).to receive(:color).and_return("white")
+    end
+
+    it "returns all & only legal moves for a knight with no pieces blocking it" do
+      expected = [[2, 2], [1, 3], [1, 5], [2, 6], [4, 6], [5, 5], [5, 3], [4, 2]]
+
+      moves = Logic.knight_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a knight near the edge of the board" do
+      expected = [ [0, 4], [2, 4], [3, 5], [3, 7] ]
 
+      moves = Logic.knight_moves(@spaces, [1, 6], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a knight blocked by allied pieces" do
+      @spaces = setup( [ [4, 2], [5, 3], [1, 3] ], "white" )
+      expected = [[2, 2], [1, 5], [2, 6], [4, 6], [5, 5]]
 
+      moves = Logic.knight_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "allows the knight to jump over other pieces" do
+      @spaces = setup( [ [2, 4], [4, 4] ], "white" )
+      @spaces = setup( [ [3, 3], [3, 5] ], "black" )
+      expected = [[2, 2], [1, 3], [1, 5], [2, 6], [4, 6], [5, 5], [5, 3], [4, 2]]
 
+      moves = Logic.knight_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a knight that can take enemy pieces" do
+      @spaces = setup( [ [4, 2], [5, 3], [1, 3] ], "black" )
+      expected = [[2, 2], [1, 3], [1, 5], [2, 6], [4, 6], [5, 5], [5, 3], [4, 2]]
 
+      moves = Logic.knight_moves(@spaces, [3, 4], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns an empty array for a knight with no legal moves" do
+      @spaces = setup( [ [1, 5], [2, 6] ], "white" )
+      expected = []
 
+      moves = Logic.knight_moves(@spaces, [0, 7], @subject)
+      expect(moves).to match_array(expected)
     end
   end
 
@@ -147,75 +244,177 @@ describe Logic do
 
   describe "#pawn_moves" do
     context "when pawn is white" do
-      it "returns both moves for an unblocked pawn that hasn't moved" do
+      before(:each) do
+        @spaces = Array.new(8) { Array.new(8) }
 
+        @subject = instance_double("piece")
+        allow(@subject).to receive(:color).and_return("white")
+      end
+
+      it "returns both moves for an unblocked pawn that hasn't moved" do
+        allow(@subject).to receive(:has_moved).and_return(false)
+        moves = Logic.pawn_moves(@spaces, [0, 1], @subject)
+        expect(moves).to match_array([[0, 2], [0, 3]])
       end
 
       it "returns one move for an unblocked pawn that has moved" do
-
+        allow(@subject).to receive(:has_moved).and_return(true)
+        moves = Logic.pawn_moves(@spaces, [2, 3], @subject)
+        expect(moves).to match_array([[2, 4]])
       end
 
       it "includes diagonal moves for a pawn that can take enemy pieces" do
-
+        @spaces = setup( [ [1, 4], [3, 4] ], "black" )
+        moves = Logic.pawn_moves(@spaces, [2, 3], @subject)
+        expect(moves).to match_array([[2, 4], [1, 4], [3, 4]])
       end
 
       it "does not allow a pawn to move or attack backwards" do
-
+        @spaces = setup( [ [1, 4], [3, 4] ], "black" )
+        moves = Logic.pawn_moves(@spaces, [2, 5], @subject)
+        expect(moves).to match_array([[2, 6]])
       end
     end
 
     context "when pawn is black" do
-      it "returns both moves for an unblocked pawn that hasn't moved" do
+      before(:each) do
+        @spaces = Array.new(8) { Array.new(8) }
 
+        @subject = instance_double("piece")
+        allow(@subject).to receive(:color).and_return("black")
+      end
+
+      it "returns both moves for an unblocked pawn that hasn't moved" do
+        allow(@subject).to receive(:has_moved).and_return(false)
+        moves = Logic.pawn_moves(@spaces, [0, 6], @subject)
+        expect(moves).to match_array([[0, 5], [0, 4]])
       end
 
       it "returns one move for an unblocked pawn that has moved" do
-
+        allow(@subject).to receive(:has_moved).and_return(true)
+        moves = Logic.pawn_moves(@spaces, [2, 4], @subject)
+        expect(moves).to match_array([[2, 3]])
       end
 
       it "includes diagonal moves for a pawn that can take enemy pieces" do
-
+        @spaces = setup( [ [1, 3], [3, 3] ], "white" )
+        moves = Logic.pawn_moves(@spaces, [2, 4], @subject)
+        expect(moves).to match_array([[2, 3], [1, 3], [3, 3]])
       end
 
       it "does not allow a pawn to move or attack backwards" do
-
+        @spaces = setup( [ [1, 4], [3, 4] ], "white" )
+        moves = Logic.pawn_moves(@spaces, [2, 3], @subject)
+        expect(moves).to match_array([[2, 2]])
       end
     end
   end
 
   describe "#queen_moves" do
-    it "returns all & only legal moves for a queen with no pieces blocking it" do
+    before(:each) do
+      @spaces = Array.new(8) { Array.new(8) }
 
+      @subject = instance_double("piece")
+      allow(@subject).to receive(:color).and_return("white")
+    end
+
+    it "returns all & only legal moves for a queen with no pieces blocking it" do
+      expected = [[0, 0], [1, 1], [2, 2], \
+                  [0, 3], [1, 3], [2, 3], \
+                  [2, 4], [1, 5], [0, 6], \
+                  [3, 4], [3, 5], [3, 6], [3, 7], \
+                  [4, 4], [5, 5], [6, 6], [7, 7], \
+                  [4, 3], [5, 3], [6, 3], [7, 3], \
+                  [4, 2], [5, 1], [6, 0], \
+                  [3, 0], [3, 1], [3, 2]]
+
+      moves = Logic.queen_moves(@spaces, [3, 3], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a queen blocked by allied pieces" do
+      @spaces = setup( [ [0, 0], [1, 3], [3, 4] ], "white" )
+      expected = [[1, 1], [2, 2], \
+                  [2, 3], \
+                  [2, 4], [1, 5], [0, 6], \
+                  [4, 4], [5, 5], [6, 6], [7, 7], \
+                  [4, 3], [5, 3], [6, 3], [7, 3], \
+                  [4, 2], [5, 1], [6, 0], \
+                  [3, 0], [3, 1], [3, 2]]
 
+      moves = Logic.queen_moves(@spaces, [3, 3], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a queen that can take enemy pieces" do
+      @spaces = setup( [ [0, 0], [1, 3], [3, 4] ], "black" )
+      expected = [[0, 0], [1, 1], [2, 2], \
+                  [1, 3], [2, 3], \
+                  [2, 4], [1, 5], [0, 6], \
+                  [3, 4], \
+                  [4, 4], [5, 5], [6, 6], [7, 7], \
+                  [4, 3], [5, 3], [6, 3], [7, 3], \
+                  [4, 2], [5, 1], [6, 0], \
+                  [3, 0], [3, 1], [3, 2]]
 
+      moves = Logic.queen_moves(@spaces, [3, 3], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns an empty array for a queen with no legal moves" do
+      @spaces = setup( [ [2, 0], [2, 1], [3, 1], [4, 1], [4, 0] ], "white" )
+      expected = []
 
+      moves = Logic.queen_moves(@spaces, [3, 0], @subject)
+      expect(moves).to match_array(expected)
     end
   end
 
   describe "rook_moves" do
-    it "returns all & only legal moves for a rook with no pieces blocking it" do
+    before(:each) do
+      @spaces = Array.new(8) { Array.new(8) }
 
+      @subject = instance_double("piece")
+      allow(@subject).to receive(:color).and_return("white")
+    end
+
+    it "returns all & only legal moves for a rook with no pieces blocking it" do
+      expected = [[0, 3], [1, 3], [2, 3], \
+                  [3, 4], [3, 5], [3, 6], [3, 7], \
+                  [4, 3], [5, 3], [6, 3], [7, 3], \
+                  [3, 0], [3, 1], [3, 2]]
+
+      moves = Logic.rook_moves(@spaces, [3, 3], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a rook blocked by allied pieces" do
+      @spaces = setup( [ [2, 3], [3, 5], [7, 3] ], "white" )
+      expected = [[3, 4], \
+                  [4, 3], [5, 3], [6, 3], \
+                  [3, 0], [3, 1], [3, 2]]
 
+      moves = Logic.rook_moves(@spaces, [3, 3], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns all & only legal moves for a rook that can take enemy pieces" do
+      @spaces = setup( [ [2, 3], [3, 5], [7, 3] ], "black" )
+      expected = [[2, 3], \
+                  [3, 4], [3, 5], \
+                  [4, 3], [5, 3], [6, 3], [7, 3], \
+                  [3, 0], [3, 1], [3, 2]]
 
+      moves = Logic.rook_moves(@spaces, [3, 3], @subject)
+      expect(moves).to match_array(expected)
     end
 
     it "returns an empty array for a rook with no legal moves" do
+      @spaces = setup( [ [0, 1], [1, 1], [1, 0] ], "white" )
+      expected = []
 
+      moves = Logic.rook_moves(@spaces, [0, 0], @subject)
+      expect(moves).to match_array(expected)
     end
   end
 
@@ -233,4 +432,16 @@ describe Logic do
     end
   end
 
+  def setup(positions, color)
+    spaces = Array.new(8) { Array.new(8) }
+
+    mock = instance_double("piece")
+    allow(mock).to receive(:color).and_return(color)
+
+    positions.each do |rank, file|
+      spaces[rank][file] = mock
+    end
+
+    return spaces
+  end
 end
